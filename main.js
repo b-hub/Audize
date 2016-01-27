c3455a32931f6c75987e5305aebb12ffe1e10c3d
b-hub <ben.magistris@gmail.com>
2016-01-27 19:37:28 +0000

    Quick and dirty combination with spring graph
    probably need to make my own....

--- main.js
+++ main.js
@@ -11,6 +11,29 @@ analyser.smoothingTimeConstant = 0; // default = 0.8
 source.connect(analyser);
 analyser.connect(audioCtx.destination);
 
+//-- SPRING GRAPH SETUP --------------------------------------
+var graph = new Springy.Graph();
+
+jQuery(function(){
+    var springy = window.springy = jQuery('#springydemo').springy({
+        graph: graph,
+        stiffness: 100.0,
+        repulsion: 400.0,
+        damping: 0.5,
+        nodeSelected: function(node){
+            console.log('Node selected: ' + JSON.stringify(node.data));
+        }
+    });
+});
+var n = 10;
+for (var i = 0; i < n; i++) graph.newNode({label: i});
+var ns = graph.nodes;
+for (var i = 1; i < n; i++) {
+    for (var j = 0; j < i; j++) {
+        graph.newEdge(ns[i], ns[j], {label: 0, length: 0, directional:false});
+    }
+}
+
 //------------------------------------------------------------
 
 // Initialising frequency / time domain buffers
@@ -20,7 +43,7 @@ var timeData = new Uint8Array( bufferLength );
 
 var can = makeCanvas(window.innerWidth, 200);
 document.querySelector("#vHolder").appendChild(can);
-var d = document.querySelector("#data");
+var info = document.querySelector("#data");
 
 var freqVisualiser = new BarGraph("f", can, frequencyData);
 var waveVisualiser = new LineGraph("t", can, timeData);
@@ -28,7 +51,7 @@ var dynamicLine = new DynamicLine("dl", can, 1, 0);
     
 var monitor = new FpsMonitor(50);
 
-var matrix = new AdjacencyMatrix(10);
+var matrix = new AdjacencyMatrix(n);
 
 //-- RENDER LOOP ----------------------------------------------
 function renderFrame() {
@@ -44,9 +67,27 @@ function renderFrame() {
     waveVisualiser.draw();
     
     matrix.add(frequencyData);
+    var ps = matrix.points;
+    for (var i = 0; i < ps.length; i++) {
+        var node = graph.nodes[i];
+        var point = ps[i];
+        node.data.label = i + "(" + point.n + ")";
+    }
+    var ds = matrix.distances;
+    for (var i = 0; i < ds.length; i++) {
+        var d = ds[i];
+        
+        if (d !== "undefined") {
+            var edge = graph.edges[i];
+            edge.data.label = Math.round(d) + 1;
+            edge.data.length = d;
+        }
+        
+    }
+        
     
 
-    d.innerHTML = monitor.getFPS();
+    info.innerHTML = monitor.getFPS();
 }
 
 
